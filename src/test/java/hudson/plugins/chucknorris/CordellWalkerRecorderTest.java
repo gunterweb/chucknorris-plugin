af932e7a14502740e8249db4f2536db6d70ae3f5
Fabien Bontemps <fab.bontemps@gmail.com>
2017-03-20 14:38:18 +0100

    Fork plugin to enable pipeline compatibility

--- src/test/java/hudson/plugins/chucknorris/CordellWalkerRecorderTest.java
+++ src/test/java/hudson/plugins/chucknorris/CordellWalkerRecorderTest.java
@@ -1,6 +1,10 @@
 package hudson.plugins.chucknorris;
 
+import static org.mockito.Matchers.same;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
@@ -10,64 +14,60 @@ import hudson.model.Build;
 import hudson.model.BuildListener;
 import hudson.model.Result;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import junit.framework.TestCase;
+import org.mockito.ArgumentCaptor;
 
 public class CordellWalkerRecorderTest extends TestCase {
 
-	private FactGenerator mockGenerator;
-	private CordellWalkerRecorder recorder;
+    private FactGenerator mockGenerator;
+    private CordellWalkerRecorder recorder;
 
-	@Override
-	public void setUp() {
-		mockGenerator = mock(FactGenerator.class);
-		recorder = new CordellWalkerRecorder(mockGenerator);
-	}
+    @Override
+    public void setUp() {
+        mockGenerator = mock(FactGenerator.class);
+        recorder = new CordellWalkerRecorder(mockGenerator);
+    }
 
-	public void testGetProjectActionWithNoLastBuildGivesNullAction() {
-		AbstractProject mockProject = mock(AbstractProject.class);
-		when(mockProject.getLastBuild()).thenReturn(null);
-		assertNull(recorder.getProjectAction(mockProject));
-	}
+    public void testGetProjectActionWithNoLastBuildGivesNullAction() {
+        AbstractProject mockProject = mock(AbstractProject.class);
+        when(mockProject.getLastBuild()).thenReturn(null);
+        assertNull(recorder.getProjectAction(mockProject));
+    }
 
-	public void testGetProjectActionHavingLastBuildGivesRoundhouseAction() {
-		AbstractProject mockProject = mock(AbstractProject.class);
-		Build mockBuild = mock(Build.class);
+    public void testGetProjectActionHavingLastBuildGivesRoundhouseAction() {
+        AbstractProject mockProject = mock(AbstractProject.class);
+        Build mockBuild = mock(Build.class);
 
-		when(mockProject.getLastBuild()).thenReturn(mockBuild);
-		when(mockBuild.getResult()).thenReturn(Result.SUCCESS);
-		when(mockGenerator.random()).thenReturn(
-				"Chuck Norris burst the dot com bubble.");
+        when(mockProject.getLastBuild()).thenReturn(mockBuild);
+        when(mockBuild.getResult()).thenReturn(Result.SUCCESS);
+        when(mockGenerator.random()).thenReturn(
+                "Chuck Norris burst the dot com bubble.");
 
-		Action action = recorder.getProjectAction(mockProject);
+        Action action = recorder.getProjectAction(mockProject);
 
-		assertTrue(action instanceof RoundhouseAction);
-		assertEquals(Style.THUMB_UP, ((RoundhouseAction) action).getStyle());
-		assertNotNull(((RoundhouseAction) action).getFact());
-	}
+        assertTrue(action instanceof RoundhouseAction);
+        assertEquals(Style.THUMB_UP, ((RoundhouseAction) action).getStyle());
+        assertNotNull(((RoundhouseAction) action).getFact());
+    }
 
-	public void testPerformWithFailureResultAddsRoundHouseActionWithBadAssStyleAndExpectedFact()
-			throws Exception {
-		List<Action> actions = new ArrayList<Action>();
-		AbstractBuild mockBuild = mock(AbstractBuild.class);
-		when(mockBuild.getResult()).thenReturn(Result.FAILURE);
-		when(mockBuild.getActions()).thenReturn(actions);
+    public void testPerformWithFailureResultAddsRoundHouseActionWithBadAssStyleAndExpectedFact()
+            throws Exception {
+        AbstractBuild mockBuild = mock(AbstractBuild.class);
+        when(mockBuild.getResult()).thenReturn(Result.FAILURE);
 
-		when(mockGenerator.random()).thenReturn(
-				"Chuck Norris burst the dot com bubble.");
+        ArgumentCaptor<RoundhouseAction> actionCaptor = ArgumentCaptor.forClass(RoundhouseAction.class);
+        doNothing().when(mockBuild).addAction(actionCaptor.capture());
 
-		assertEquals(0, actions.size());
+        when(mockGenerator.random()).thenReturn(
+                "Chuck Norris burst the dot com bubble.");
 
-		recorder.perform(mockBuild, mock(Launcher.class),
-				mock(BuildListener.class));
+        recorder.perform(mockBuild, mock(Launcher.class),
+                mock(BuildListener.class));
 
-		assertEquals(1, actions.size());
-		assertTrue(actions.get(0) instanceof RoundhouseAction);
-		assertEquals(Style.BAD_ASS, ((RoundhouseAction) actions.get(0))
-				.getStyle());
-		assertEquals("Chuck Norris burst the dot com bubble.",
-				((RoundhouseAction) actions.get(0)).getFact());
-	}
+        RoundhouseAction action = actionCaptor.getValue();
+
+        verify(mockBuild, times(1)).addAction(same(action));
+        assertEquals(Style.BAD_ASS, (action).getStyle());
+        assertEquals("Chuck Norris burst the dot com bubble.", action.getFact());
+    }
 }
